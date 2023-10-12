df476e7a59b48978dffbbf33d1ad5ef61f10b104
livia-pimentel <livia_macedo89@yahoo.com.br>
2023-10-12 06:42:21 -0300

    update

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 /* ***********************
  * Require Statements
  *************************/
+const baseController = require("./controllers/baseController")
 const express = require("express")
 const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute") //está certo?
 
 /* ***********************
  * View Engine and Templates
@@ -24,9 +26,11 @@ app.set("layout", "./layouts/layout") // not at views root
 app.use(static)
 
 // Index route
-app.get("/", function(req, res){
-  res.render("index", {title: "Home"})
-})
+// app.get("/", function(req, res){      //previous route
+//   res.render("index", {title: "Home"})
+// })
+app.get("/", baseController.buildHome) //alter in week 5
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
