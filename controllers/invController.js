fafdaf7b2b30ae082be9d70a94b23744bd12fccd
livia-pimentel <livia_macedo89@yahoo.com.br>
2023-10-21 22:18:23 -0300

    Update

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,5 +19,21 @@ invCont.buildByClassificationId = async function (req, res, next) {
   })
 }
 
+/* ***************************
+ *  Build inventory by item detail view
+ * ************************** */
+invCont.buildClassificationGrid= async function (req, res, next) {
+  const classification_id = req.params.classificationId
+  const data = await invModel.getInventoryByClassificationId(classification_id)
+  const grid = await utilities.buildClassificationGrid(data)
+  let nav = await utilities.getNav()
+  const className = data[0].classification_name
+  res.render("./inventory/classification", {
+    title: className + " vehicles",
+    nav,
+    grid,
+  })
+}
+
 
 module.exports = invCont
